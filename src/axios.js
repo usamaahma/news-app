a687b00d687f0b645383010c6908aa4ce4d44496
usamaahma <usamaahmad087@gmail.com>
2024-07-31 13:11:43 +0500

    errors resolved

--- src/axios.js
+++ src/axios.js
@@ -3,11 +3,17 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 const BASE_URL = 'https://newsapi.org/v2';
 
-const fetchNews = async (query = 'general') => { 
+const fetchNews = async (query = '') => {
+  const url = `${BASE_URL}/top-headlines?country=us&q=${query}&apiKey=${API_KEY}`;
+
   try {
-    const url = `${BASE_URL}/top-headlines?q=${query}&apiKey=${API_KEY}`;
     const response = await axios.get(url);
-    return response.data.articles;
+    if (response.status === 200) {
+      return response.data.articles;
+    } else {
+      console.error(`Error: ${response.statusText}`);
+      throw new Error(`Error: ${response.statusText}`);
+    }
   } catch (error) {
     console.error('Error fetching news:', error.response ? error.response.data : error.message);
     throw error;
